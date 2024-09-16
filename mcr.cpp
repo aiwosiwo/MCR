d791b7a62fa164f388b89f7511074b974658307e
aiwosiwo <57023534+aiwosiwo@users.noreply.github.com>
2024-09-16 22:34:00 +0800

    Update mcr.cpp
    - Variables ‘player1’ and ‘player2’ are deleted
    - Logical error in determining a tie is corrected 
    - IsWin function is simplified
    - Add repeated judgment and range judgment on the input
    - Correct input hints
    - Simplify output of final grid

--- mcr.cpp
+++ mcr.cpp
@@ -2,48 +2,65 @@
 using namespace std;
 
 bool isWin(char game[3][3]){
-bool win = false; 
-    if (game[0][0] == game[0][1] && game[0][1] == game[0][2] && (game[0][0] == 'X' || game[0][0] == 'O')) win = true; 
-    if (game[1][0] == game[1][1] && game[1][1] == game[1][2] && (game[1][0] == 'X' || game[1][0] == 'O')) win = true; 
-    if (game[2][0] == game[2][1] && game[2][1] == game[2][2] && (game[2][0] == 'X' || game[2][0] == 'O')) win = true; 
+    if (game[0][0] == game[0][1] && game[0][1] == game[0][2] && (game[0][0] == 'X' || game[0][0] == 'O')) return true;
+    if (game[1][0] == game[1][1] && game[1][1] == game[1][2] && (game[1][0] == 'X' || game[1][0] == 'O')) return true; 
+    if (game[2][0] == game[2][1] && game[2][1] == game[2][2] && (game[2][0] == 'X' || game[2][0] == 'O')) return true;
     // column
-    if (game[0][0] == game[1][0] && game[1][0] == game[2][0] && (game[0][0] == 'X' || game[0][0] == 'O')) win = true; 
-    if (game[0][1] == game[1][1] && game[1][1] == game[2][1] && (game[0][1] == 'X' || game[0][1] == 'O')) win = true; 
-    if (game[0][2] == game[1][2] && game[1][2] == game[2][2] && (game[0][2] == 'X' || game[0][2] == 'O')) win = true; 
+    if (game[0][0] == game[1][0] && game[1][0] == game[2][0] && (game[0][0] == 'X' || game[0][0] == 'O')) return true;
+    if (game[0][1] == game[1][1] && game[1][1] == game[2][1] && (game[0][1] == 'X' || game[0][1] == 'O')) return true; 
+    if (game[0][2] == game[1][2] && game[1][2] == game[2][2] && (game[0][2] == 'X' || game[0][2] == 'O')) return true; 
     // diagonal 
-    if (game[0][0] == game[1][1] && game[1][1] == game[2][2] && (game[0][0] == 'X' || game[0][0] == 'O')) win = true; 
-    if (game[0][2] == game[1][1] && game[1][1] == game[2][0] && (game[0][2] == 'X' || game[0][2] == 'O')) win = true; 
-    return win;
-} 
+    if (game[0][0] == game[1][1] && game[1][1] == game[2][2] && (game[0][0] == 'X' || game[0][0] == 'O')) return true; 
+    if (game[0][2] == game[1][1] && game[1][1] == game[2][0] && (game[0][2] == 'X' || game[0][2] == 'O')) return true; 
+    return false;
+}
+
+bool isFill(char game[3][3],int i,int j){
+    if (game[i][j] == 'X' || game[i][j] == 'O')
+        return true;
+    return false;
+}
+
 int main(){
-    int i, j;  
+    int i, j;
     char game[3][3] = {' '}; // Tic-tac-toe
     char player1 = 'X'; 
     char player2 = 'O'; 
     bool turn = true; // false for player 1's turn, true for player 2's turn. Player 1 first.
     cout << "X = Player 1" << endl << "O = Player 2" << endl;
-    for (int n=0; n<9; n++){
+    int n;
+    for (n=0; n<9; n++){
         turn = !turn; // use the not-operator to change true to false or false to true.
         if (turn == false) 
             cout << "Player 1: "; 
         else
             cout << "Player 2: "; 
-        cout << "Which cell to mark? i:[1..3], j:[1..3]: "; 
+        cout << "Which cell to mark? i:[0..2], j:[0..2]: "; 
         cin >> i >> j;
-        if (turn == false) 
+        while (i < 0 || i > 2 || j < 0 || j > 2){ //range judgment
+            cout << "Out of range, input again: ";
+            cin >> i >> j;
+        }
+        while (isFill(game, i, j)){ //repeat judgment
+            cout << "Already filled, input again: ";
+            cin >> i >> j;
+        }
+        if (turn == false)
             game[i][j] = 'X';
-        else 
-            game[i][j] = 'O'; 
+        else
+            game[i][j] = 'O';
         if (isWin(game)){
             cout << "Win!" << endl;
-        break; // need to terminate the problem
-        } 
-    } 
-    if (i==3) // all celles with i=0 to 2 have been inputted above but no winner yet
-    cout << "Tie!" << endl;
-    // show the game to console
-    cout << game[0][0] << " " << game[0][1] << " " << game[0][2] << endl;
-    cout << game[1][0] << " " << game[1][1] << " " << game[1][2] << endl;
-    cout << game[2][0] << " " << game[2][1] << " " << game[2][2] << endl;
-    return 0; 
+            break;
+        }
+    }
+    if (n==9)
+        cout << "Tie!" << endl;
+    for (int p=0; p<3; p++){
+        for (int q=0; q<3; q++){
+            cout << game[p][q] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
 }
